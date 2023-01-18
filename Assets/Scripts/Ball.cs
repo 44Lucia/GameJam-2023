768586c713c0361988c02039538f1557977e98e2
marKinhuENTI <95031286+Koaladelamor@users.noreply.github.com>
2023-01-18 20:50:04 +0100

    Balls

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    private Vector3 finalVelocity;
+    private float speed;
+    private Rigidbody2D rb;
+    [SerializeField] private float initialForce;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb.AddForce(transform.right * initialForce);
+    }
+
+    // Update is called once per frame
+    /*private void Update()
+    {
+           
+    }*/
+
+}
---
+++ Assets/Scripts/DynamicBalls.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicBalls : MonoBehaviour
+{
+    [SerializeField] private GameObject upMaxPosition;
+    [SerializeField] private GameObject downMaxPosition;
+
+    [SerializeField] private int ballID;
+    [SerializeField] private float yDirection;
+    [SerializeField] private float speed;
+    [SerializeField] private float maxPositionOffset;
+
+    private void Awake()
+    {
+        //Instantiate max positions
+        upMaxPosition = new GameObject("DynamicBall" + ballID + "_UpMaxPosition");
+        downMaxPosition = new GameObject("DynamicBall" + ballID + "_DownMaxPosition");
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //Set Max Positions
+        upMaxPosition.transform.position = this.transform.position;
+        upMaxPosition.transform.rotation = this.transform.rotation;
+        upMaxPosition.transform.position = new Vector3(upMaxPosition.transform.position.x, upMaxPosition.transform.position.y + maxPositionOffset, upMaxPosition.transform.position.z);
+
+        downMaxPosition.transform.position = this.transform.position;
+        downMaxPosition.transform.rotation = this.transform.rotation;
+        downMaxPosition.transform.position = new Vector3(downMaxPosition.transform.position.x, downMaxPosition.transform.position.y - maxPositionOffset, downMaxPosition.transform.position.z);
+
+        this.gameObject.name = "DynamicBall_" + ballID.ToString();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += Vector3.up * yDirection * speed * Time.deltaTime;
+
+        if (yDirection == 1 && transform.position.y >= upMaxPosition.transform.position.y) 
+        {
+            yDirection = -1;
+        }
+        if (yDirection == -1 && transform.position.y <= downMaxPosition.transform.position.y)
+        {
+            yDirection = 1;
+        }
+    }
+}
