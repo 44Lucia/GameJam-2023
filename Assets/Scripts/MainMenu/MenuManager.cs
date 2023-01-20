4049a74d0b03560e1a37238ab9d10480a9465205
Lucia <95747188+44Lucia@users.noreply.github.com>
2023-01-20 17:28:30 +0100

    Main Menu finished

--- Assets/Scripts/MainMenu/MenuManager.cs
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -12,6 +12,8 @@ namespace Game
         private GameObject pnl_settings;
         private GameObject pnl_credits;
 
+        private GameObject pnl_controls;
+
         Scene m_currentScene = null;
 
         private void Awake()
@@ -31,13 +33,11 @@ namespace Game
             {
                 pnl_credits.SetActive(false);
             }
+            if (pnl_controls != null)
+            {
+                pnl_controls.SetActive(false);
+            }
         }
-
-        // Update is called once per frame
-        /*private void Update()
-        {
-
-        }*/
 
         private void GetReferences()
         {
@@ -58,12 +58,19 @@ namespace Game
             {
                 pnl_credits = tempGO;
             }
+
+            tempGO = GameObject.Find("pnl_Controls");
+            if (tempGO != null)
+            {
+                pnl_controls = tempGO;
+            }
         }
 
         public void OpenMainMenu()
         {
             pnl_credits.SetActive(false);
             pnl_settings.SetActive(false);
+            pnl_controls.SetActive(false);
             pnl_mainMenu.SetActive(true);
             SoundManager.Instance.PlayOnce(AudioClipName.CLICK_MENU);
         }
@@ -72,6 +79,7 @@ namespace Game
         {
             pnl_credits.SetActive(true);
             pnl_settings.SetActive(false);
+            pnl_controls.SetActive(false);
             pnl_mainMenu.SetActive(false);
             SoundManager.Instance.PlayOnce(AudioClipName.CLICK_MENU);
         }
@@ -80,6 +88,16 @@ namespace Game
         {
             pnl_credits.SetActive(false);
             pnl_settings.SetActive(true);
+            pnl_controls.SetActive(false);
+            pnl_mainMenu.SetActive(false);
+            SoundManager.Instance.PlayOnce(AudioClipName.CLICK_MENU);
+        }
+
+        public void OpenControls() 
+        {
+            pnl_credits.SetActive(false);
+            pnl_settings.SetActive(false);
+            pnl_controls.SetActive(true);
             pnl_mainMenu.SetActive(false);
             SoundManager.Instance.PlayOnce(AudioClipName.CLICK_MENU);
         }
