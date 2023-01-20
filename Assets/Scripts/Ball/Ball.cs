d05e9c2a07cd9670ffead8825427e5d3b3cccae8
marKinhu <95031286+Koaladelamor@users.noreply.github.com>
2023-01-20 15:28:23 +0100

    Ball random initial direction

---
+++ Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : Singleton<Ball>
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    [SerializeField] private float initialForce;
+    [SerializeField] private Transform currentInitPos;
+    [SerializeField] private Transform initPosPlayer1;
+    [SerializeField] private Transform initPosPlayer2;
+
+    [SerializeField] private float minRandomDir;
+    [SerializeField] private float maxRandomDir;
+
+    //[SerializeField] private bool targetIsPlayer1;
+    [SerializeField] private bool targetIsPlayer2;
+
+
+    private void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SetInitPosition();
+        AddForceWithRandomDirection();
+    }
+
+    private void AddForceWithRandomDirection() 
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, Random.Range(minRandomDir, maxRandomDir));
+        //Debug.Log(transform.eulerAngles.z);
+        Vector3 force = -transform.right;
+        if (targetIsPlayer2) { force = transform.right; }
+        rb.AddForce(force * initialForce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //bounce
+        SoundManager.Instance.PlayOnce(AudioClipName.BOUNCE_BALL);
+        animator.SetBool("Idle", false);
+    }
+
+    public void SetInitPosition() 
+    {
+        currentInitPos = initPosPlayer1;
+        if (targetIsPlayer2) { currentInitPos = initPosPlayer2; }
+        this.transform.position = currentInitPos.position;
+    }
+
+    public void SetAnimatorIdleToTrue() { animator.SetBool("Idle", true); }
+
+}
